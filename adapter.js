bd76dffa7fcf6fc08c7b72e85673c7856332396f
taifshah <45360567+taifshah@users.noreply.github.com>
2019-01-02 02:37:36 +0500

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -19,7 +19,7 @@ class Adapter {
     }
 
     reconnect() {
-        this.connection.api.setOptions({ url: `wss://news.weku.io:8190` });
+        this.connection.api.setOptions({ url: `wss://standby.weku.io:8190` });
         this.connection.config.set(`address_prefix`, `WKA`);
         this.connection.config.set(`chain_id`, `b24e09256ee14bab6d58bfa3a4e47b0474a73ef4d6c47eeea007848195fa085e`);
     }
